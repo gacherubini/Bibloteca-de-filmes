b13c0103e10408f6a8fc89e20c76d1e73da8933c
Gabriel <bielcheeeeee@gmail.com>
2023-09-09 20:15:29 -0300

    commit da bibloteca pronta

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+import "./App.css"
+
+import NavBar from "./components/NavBar"
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <Outlet/>
+    </div>
+  )
+  
+}
+
+export default App
